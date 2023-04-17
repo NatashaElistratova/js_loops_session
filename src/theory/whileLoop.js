1bd9c73737a565178aef2c46a7256b5804160936
natalia.yelistratova <natalia.yelistratova@gmail.com>
2023-04-17 14:01:20 +0300

    While loop

---
+++ src/theory/whileLoop.js
@@ -0,0 +1,19 @@
+let i = 0;
+while (i < 3) { // выводит 0, затем 1, затем 2
+  console.log(i);
+  i++;
+}
+
+
+let j = 3;
+while (j) { // когда i будет равно 0, условие станет ложным, и цикл остановится
+    console.log(j);
+  j--;
+}
+
+let x = 0;
+do {
+  console.log(x);
+  x++;
+} while (x < 3);
+
